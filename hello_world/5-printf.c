8c8598ffe86d310f99bd3f975242e0b3059fe3d0
yarielvegatorres <yarielvegatorres@gmail.com>
2024-10-22 11:27:12 -0400

    hello,printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints "Programming is like building a multilingual puzzle,"
+ * followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
