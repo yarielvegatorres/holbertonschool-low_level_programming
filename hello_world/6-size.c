662712fb17bb6d12ef4796f02713dfe023c17858
yarielvegatorres <yarielvegatorres@gmail.com>
2024-10-22 14:33:44 -0400

    size is not granduer

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
+
 /**
- * main - Aprogram that prints the size of various computer types
- *return 0 (success)
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
  */
+
 int main(void)
+
 {
-	printf("size of a char: %lu bytes(s)\n", sizeof(char));
-	printf("size of a int: %lu bytes(s)\n", sizeof(int));
-	printf("size of a long int: %lu bytes (s)\n", sizeof(long int));
-	printf("size of a long long int: %lu bytes (s)\n", sizeof(long long int));
-	printf("size of a float: %lu bytes(s) \n", sizeof(float));
+	printf("size of a char: %zu bytes(s)\n", sizeof(char));
+	printf("size of an int: %zu bytes(s)\n", sizeof(int));
+	printf("size of a long int: %zu bytes(s)\n", sizeof(long int));
+	printf("size of a long long int: %zu bytes(s)\n", sizeof(long long int));
+	printf("size of a float: %zu bytes(s)\n", sizeof(float));
 	return (0);
 }
-
