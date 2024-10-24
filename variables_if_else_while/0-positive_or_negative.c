ff58d5aba0623c42747326002191cd65e0a6ca7b
yarielvegatorres <yarielvegatorres@gmail.com>
2024-10-24 18:51:08 -0400

    positive anything is better

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void) 
+
+{
+	int n = 0;
+
+	/* your code goes there */
+	
+	if (n > 0){
+	printf("is postive\n");}
+	else if (n==0){
+		printf("is zero\n");}
+	else {
+		printf("is negative\n");}
+	return (0);
+}
+
