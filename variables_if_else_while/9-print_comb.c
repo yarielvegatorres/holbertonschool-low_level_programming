48f011e946589351603a3052a0ae06af9ce89245
yarielvegatorres <yarielvegatorres@gmail.com>
2024-10-26 13:11:38 -0400

    patience

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -9,10 +9,17 @@ int main(void)
 	int num;
 
 	for (num = 0; num < 10; num++)
+	{
 	putchar (num + '0');
+
 	if (num != 9)
+	{
 		putchar (',');
+
 	putchar (' ');
+	}
+}
 	putchar('\n');
+
 	return (0);
 }
