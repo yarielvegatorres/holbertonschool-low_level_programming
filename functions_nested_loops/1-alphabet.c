7141b0542e8e70ff14bdf37869395fa96bc85f95
yarielvegatorres <yarielvegatorres@gmail.com>
2024-10-27 23:00:42 -0400

    alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+void print_alphabet(void);
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+print_alphabet();
+return (0);
+}
+
+void print_alphabet(void)
+{
+int letter;
+
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+putchar(letter);
+}
+putchar('\n');
+}
