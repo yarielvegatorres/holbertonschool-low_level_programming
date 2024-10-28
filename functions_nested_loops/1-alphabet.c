07023076ed9d7a66ff7e748a6ebfd2613f8757f6
yarielvegatorres <yarielvegatorres@gmail.com>
2024-10-27 23:11:18 -0400

    alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 void print_alphabet(void);
 
 /**
@@ -10,4 +11,14 @@ int main(void)
 {
 	print_alphabet();
 	return (0);
+}
+void print_alphabet(void)
+{
+	int letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar (letter);
+	}
+		putchar ('\n');
 }
