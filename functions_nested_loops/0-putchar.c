e8861f77dcf0a5371d1028b6f3d61922aa787f48
yarielvegatorres <yarielvegatorres@gmail.com>
2024-10-27 23:34:09 -0400

    putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
+#include "main.h"
+
 /**
- * main - Prints "_putchar" followed by a new line
+ * main - Entry point of the program
  *
  * Return: Always 0 (success)
  */
 int main(void)
+
 {
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
-	return (0);
+putchar ('_');
+putchar ('p');
+putchar ('u');
+putchar ('t');
+putchar ('c');
+putchar ('h');
+putchar ('a');
+putchar ('r');
+putchar ('\n');
+
+return (0);
 }
